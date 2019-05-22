9be44b7db2d07a203e80a5438bfd307ec74846ee
Shameena Bonomally <shameena.bonomally@gmail.com>
2019-05-22 20:09:51 +0200

    DY job

--- Taggers/test/sig_jobs.sh
+++ Taggers/test/sig_jobs.sh
@@ -2,5 +2,5 @@ queue="testmatch"
 useAAA=0
 version="reoptimisedClean"
 
-fggRunJobs.py --load DoubleEG_ReMiniAOD_2_5_Y.json -d DY_plots -x cmsRun  vbf_dumper_2017.py runOnZee=True maxEvents=-1 -q $queue -D -P metaConditions=$CMSSW_BASE/src/flashgg/MetaData/data/MetaConditions/Era2016_RR-17Jul2018_v1.json
+fggRunJobs.py --load DoubleEG_ReMiniAOD_2_5_Y.json -d DY_plots -x cmsRun  vbf_dumper_2017.py runOnZee=True maxEvents=-1 -q $queue -D -P useAAA=$useAAA metaConditions=$CMSSW_BASE/src/flashgg/MetaData/data/MetaConditions/Era2016_RR-17Jul2018_v1.json
 
